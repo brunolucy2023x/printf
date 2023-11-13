67ee1773d6381e4f0f2127dd6032da62ec0d9f54
brunolucy2023x <lucyb5147@gmail.com>
2023-11-13 07:32:44 -0800

    Group Work

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,50 +1,31 @@
 #include "main.h"
 
-#define MAX_HEX_DIGITS 16
-
 /**
- * printf_pointer - prints a binary number
- * @args: numberof arguements
- * @printed: the printed characters
- * Return: printed charcaters
+ * printf_pointer - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
  */
-
-int printf_pointer(va_list args, int printed)
+int printf_pointer(va_list val)
 {
-	void *ptr = va_arg(args, void*);
-	unsigned long num = (unsigned long) ptr;
-	int digits = 0;
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
 	int i;
-	unsigned long temp = num;
-	char hex_digits[MAX_HEX_DIGITS] = "0123456789abcdef";
-	char hex[MAX_HEX_DIGITS];
 
-	while (temp != 0)
+	p = va_arg(val, void*);
+	if (p == NULL)
 	{
-		digits++;
-		temp /= 16;
-	}
-
-	printed += _putchar('0');
-	printed += _putchar('x');
-
-	if (num == 0)
-	{
-		printed += _putchar('0');
-	}
-	else
-	{
-		for (i = digits - 1; i >= 0; i--)
+		for (i = 0; s[i] != '\0'; i++)
 		{
-			int digit = num % 16;
-
-			hex[i] = hex_digits[digit];
-			num /= 16;
+			_putchar(s[i]);
 		}
-		for (i = 0; i < digits; i++)
-		{
-			printed += _putchar(hex[i]);
-		}
+		return (i);
 	}
-	return (printed);
+
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = printf_hex_aux(a);
+	return (b + 2);
 }
